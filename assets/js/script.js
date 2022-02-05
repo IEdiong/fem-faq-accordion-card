a262795bf14cd4d3c3cff4f39cf704c4535511c6
IEdiong <ediongi6@hotmail.com>
2022-02-05 19:21:44 +0100

    Fixed the accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,9 @@
 let accordionBtns = document.getElementsByClassName('accordion__btn');
 
 Array.from(accordionBtns).forEach((accordionBtn) => {
-    
-    console.log(accordionBtn);
+
     accordionBtn.onclick = function() {
-        let content = this.nextElementSibling;
-        console.log(content);
+        console.log(accordionBtn.parentElement.nextElementSibling);
+        this.parentElement.nextElementSibling.classList.toggle('open');
     }
 })
